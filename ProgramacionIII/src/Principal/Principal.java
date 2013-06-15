c6fd2e40e7f616c48d369b7e02b3b216145ffbaa
fernandodanielrusso@gmail.com <fernandodanielrusso@gmail.com@65f6a111-1eb7-77d9-791d-0422c8d6fff4>
2013-06-15 21:10:28 +0000

    El ejercicio 4 está mal planteado, debe hacerse la modificación para que pueda volver a visitar otros vértices.

--- ProgramacionIII/src/Principal/Principal.java
+++ ProgramacionIII/src/Principal/Principal.java
@@ -1,5 +1,9 @@
 package Principal;
 import Implementacion.*;
+import Implementaciones.Cola;
+import TDA.ColaTDA;
+import TDA.ConjuntoTDA;
+import TDA.GrafoDirTDA;
 
 public class Principal {
 
@@ -7,7 +11,26 @@ public class Principal {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		Ejercicios miEjercicio = new Ejercicios();
+		GrafoDirTDA<String> miEjercicio = Ejercicios.cargarGrafo();
+		
+		ConjuntoTDA<String> vertices = miEjercicio.Vertices();
+
+//		while (!vertices.conjuntoVacio()) {
+//			String k = vertices.elegir();
+//			vertices.sacar(k);
+//			ConjuntoTDA<String> ady = miEjercicio.Adyacentes(k);
+//			while (!ady.conjuntoVacio()) {
+//				String i = ady.elegir();
+//				ady.sacar(i);
+//				System.out.println(k + "-->" + i + " Peso: "
+//						+ miEjercicio.PesoArista(k, i));
+//			}
+//
+//		}
+		
+	
+		Ejercicios.Ejercicio4(miEjercicio, "a", 7);
+	
 	}
 
 }
