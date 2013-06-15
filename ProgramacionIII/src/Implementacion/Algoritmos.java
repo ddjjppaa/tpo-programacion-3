14c0b6c22ec030f6014bfcb8ae786f3b29b4f0d0
fernandodanielrusso <fernandodanielrusso@gmail.com>
2013-06-15 22:57:05 +0000

    Se agregaron métodos (Floyd PD, y Ejercicio3D).
    El algoritmo del Ejercicio 5 aparentemente funciona, pero tiene un problema al hacer la copia de cada etapa del vector solución. Es por esto que a veces no trae solución y a veces si. REVISAR.
    
    Nota: para hacer mejor el control, habría que agregar impresiones de las soluciones parciales.
    
    Adicionalmente, hay que agregar, una vez realizado ese arreglo, un booleano que determine que ya encontró una solución válida para que corte el backtracking.

--- ProgramacionIII/src/Implementacion/Algoritmos.java
+++ ProgramacionIII/src/Implementacion/Algoritmos.java
@@ -18,9 +18,63 @@ public class Algoritmos {
 	}
 	
 	// Ejercicio 3C
-	static GrafoDirTDA<String> FloydPD(GrafoDirTDA<String> g){
-		
-		return null;
+	public static GrafoDirTDA<String> FloydPD(GrafoDirTDA<String> g) {
+		ConjuntoTDA<String> conjuntoI, conjuntoJ, conjuntoK;
+		String i, j, k;
+		GrafoDirTDA<String> r = new GrafoDir<String>();
+		r.InicializarGrafo();
+
+		conjuntoK = g.Vertices();
+		while (!conjuntoK.conjuntoVacio()) {
+			k = conjuntoK.elegir();
+			conjuntoK.sacar(k);
+			r.AgregarVertice(k);
+		}
+
+		conjuntoK = g.Vertices();
+		while (!conjuntoK.conjuntoVacio()) {
+			k = conjuntoK.elegir();
+			conjuntoK.sacar(k);
+			conjuntoI = g.Adyacentes(k);
+			while (!conjuntoI.conjuntoVacio()) {
+				i = conjuntoI.elegir();
+				conjuntoI.sacar(i);
+				r.AgregarArista(k, i, g.PesoArista(k, i));
+			}
+		}
+
+		conjuntoK = g.Vertices();
+		while (!conjuntoK.conjuntoVacio()) {
+			k = conjuntoK.elegir();
+			conjuntoK.sacar(k);
+			conjuntoI = g.Vertices();
+			conjuntoI.sacar(k);
+			while (!conjuntoI.conjuntoVacio()) {
+				i = conjuntoI.elegir();
+				conjuntoI.sacar(i);
+				if (r.ExisteArista(i, k)) {
+					conjuntoJ = r.Adyacentes(k);
+					conjuntoJ.sacar(i);
+					while (!conjuntoJ.conjuntoVacio()) {
+						j = conjuntoJ.elegir();
+						conjuntoJ.sacar(j);
+						if (r.ExisteArista(i, j)) {
+							if (r.PesoArista(i, k) + r.PesoArista(k, j) < r
+									.PesoArista(i, j)) {
+								r.AgregarArista(i, j,
+										r.PesoArista(i, k) + r.PesoArista(k, j));
+							}
+						} else {
+							r.AgregarArista(i, j,
+									r.PesoArista(i, k) + r.PesoArista(k, j));
+						}
+					}
+				}
+			}
+		}
+
+		return r;
+
 	}
 	
 	
